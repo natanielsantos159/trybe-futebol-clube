659557b7f81416a7c867539129fd586358e859e3
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-19 11:47:35 -0300

    Adiciona listen no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,9 +5,8 @@ class App {
   // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
   }
 
   private config():void {
@@ -24,7 +23,7 @@ class App {
 
   // ...
   public start(PORT: string | number):void {
-    // ...
+    this.app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
   }
 }
 
