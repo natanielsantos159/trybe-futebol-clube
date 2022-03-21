6577a06520fac3a8dcf12d17070ea514f2fb62e3
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-20 22:20:39 -0300

    Requisito 4

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,31 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Users from '../database/models/Users';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testa a rota /login', () => {
+
+  let chaiHttpResponse: Response;
+
+  it('retorna erro quando passa informações de usuário não cadastrado', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'test@example.com', password: 'test1234'})
+
+    expect(chaiHttpResponse.status).equal(404);
+    expect(chaiHttpResponse.error).equal('User not found');
+  });
+
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
