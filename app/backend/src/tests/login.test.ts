d862ff8d4feb0b0c4536ee4b21b51bd1cc78080e
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-23 15:00:05 -0300

    Refatora testes

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -20,22 +20,22 @@ const usersMock = {
     username: 'User',
     role: 'user',
     email: 'user@user.com',
-    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
   },
   admin: {
     id: 1,
     username: 'Admin',
     role: 'admin',
     email: 'admin@admin.com',
-    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
   }
 }
 
-const usersMockDecodedPasswords = {
+const usersMockPasswords = {
   user: {
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
     decodedPassword: 'secret_user',
   },
   admin: {
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
     decodedPassword: 'secret_admin',
   }
 }
@@ -47,10 +47,9 @@ describe('Testa a rota /login', () => {
   let chaiHttpResponse: Response;
 
   before(() => {
-    console.log('cheguei')
     sinon
       .stub(Users, 'findOne')
-      .resolves(usersMock.user as Users);
+      .resolves({...usersMock.user, password: usersMockPasswords.user.password} as Users);
 
     sinon
       .stub(jwtHelper, 'getNewToken')
@@ -64,37 +63,28 @@ describe('Testa a rota /login', () => {
     jwtStub.restore();
   })
 
+  const callWithBody = (body: object) => {
+     return chai.request(app).post('/login').send(body)
+  }
+
+  it('retorna um token e informações do usuario quando é feito login de um usuário normal', async () => {
+    chaiHttpResponse = await callWithBody({ email: usersMock.user.email, password: usersMockPasswords.user.decodedPassword })
+    
+    expect(chaiHttpResponse.status).equal(200);
+    expect(chaiHttpResponse.body).to.be.an('object');
+    expect(chaiHttpResponse.body).to.be.have.keys(['user', 'token']);
+    expect(chaiHttpResponse.body.token).to.be.equal(validToken);
+    expect(chaiHttpResponse.body.user).to.have.all.keys(['email', 'id', 'role', 'username']);
+    expect(chaiHttpResponse.body.user).to.deep.equal(usersMock.user);
+
+  });
   it('retorna erro quando passa informações de usuário não cadastrado', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({ email: 'test@example.com', password: 'test1234' })
+    chaiHttpResponse = await callWithBody({ email: 'test@example.com', password: 'test1234' })
 
     expect(chaiHttpResponse.status).equal(401);
     expect(chaiHttpResponse.body.message).equal('Incorrect email or password');
   });
 
-  it('retorna um token e informações do usuario quando é feito login de um usuário normal', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({ email: usersMock.user.email, password: usersMockDecodedPasswords.user.decodedPassword })
-
-    expect(chaiHttpResponse.status).equal(200);
-    expect(chaiHttpResponse.body).to.be.an('object');
-    expect(chaiHttpResponse.body).to.be.have.keys(['user', 'token']);
-    expect(chaiHttpResponse.body.token).to.be.equal(validToken);
-    expect(chaiHttpResponse.body.user).to.be.have.keys(['email', 'id', 'role', 'username']);
-    expect(chaiHttpResponse.body.user.email).to.be.an('string');
-    expect(chaiHttpResponse.body.user.email).to.be.equal(usersMock.user.email);
-    expect(chaiHttpResponse.body.user.id).to.be.an('number');
-    expect(chaiHttpResponse.body.user.id).to.be.equal(usersMock.user.id);
-    expect(chaiHttpResponse.body.user.username).to.be.an('string');
-    expect(chaiHttpResponse.body.user.username).to.be.equal(usersMock.user.username);
-    expect(chaiHttpResponse.body.user.role).to.be.an('string');
-    expect(chaiHttpResponse.body.user.role).to.be.equal(usersMock.user.role);
-
-  });
 });
 
 describe('Testa a rota /login/validate', () => {
