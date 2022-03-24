d525a19346c8a23b18460bee99f9f1727c6cb5a8
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-24 16:17:27 -0300

    Requisito 23 - Cria rota para cadastro de partida

--- app/backend/src/routes/matchsRoute.ts
+++ app/backend/src/routes/matchsRoute.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import validateMatchInfo from "../middlewares/validateMatchInfo";
 import matchsController from "../controllers/matchsController";
+import validateJWT from "../middlewares/validateJWT";
 
 const matchs = Router();
 
 matchs.get('/', matchsController.getAll);
+
+matchs.post('/', validateMatchInfo, validateJWT, matchsController.create);
 
 export default matchs;
