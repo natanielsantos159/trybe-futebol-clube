31083be33b4d52174010525717032284327fde48
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-24 16:14:51 -0300

    Abstrai lógica de checagem de senhas no service do user

--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -3,14 +3,18 @@ import Users from "../database/models/Users";
 import UserLoginInfo from "../interfaces/UserLoginInfo";
 import * as bcrypt from 'bcryptjs';
 
+const checkPassword = (password: string, hash: string): boolean => {
+  const passwordIsCorrect = bcrypt.compareSync(password, hash);
+  return passwordIsCorrect;
+}
+
 const getUser = async ({ email, password }: UserLoginInfo): Promise<IUser> => {
   const defaultError = new Error("Incorrect email or password");
 
   const foundUser = await Users.findOne({ where: { email } });
   if (!foundUser) throw defaultError;
 
-  const passwordIsCorrect = bcrypt.compareSync(password, foundUser.password);
-  if (!passwordIsCorrect) throw defaultError;
+  if (!checkPassword(password, foundUser.password)) throw defaultError;
 
   const { id, role, username } = foundUser;
   return { email, id, role, username };
