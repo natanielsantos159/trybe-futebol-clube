3b744a98da3cd7366d1a405493c3e1cdff0fb854
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-23 21:31:16 -0300

    Refatora testes da rota /clubs

--- app/backend/src/tests/clubs.test.ts
+++ app/backend/src/tests/clubs.test.ts
@@ -4,7 +4,6 @@ import 'mocha';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import jwtHelper from '../utils/jwtHelper'
 
 import { Response } from 'superagent';
 import Sinon = require('sinon');
@@ -16,47 +15,6 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 const clubsMock: IClub[] = [{
-  id: 1,
-  club_name: "Avaí/Kindermann"
-},
-{
-  id: 2,
-  club_name: "Bahia"
-},
-{
-  id: 3,
-  club_name: "Botafogo"
-},
-{
-  id: 4,
-  club_name: "Corinthians"
-},
-{
-  id: 5,
-  club_name: "Cruzeiro"
-},
-{
-  id: 6,
-  club_name: "Ferroviária"
-},
-{
-  id: 7,
-  club_name: "Flamengo"
-},
-{
-  id: 8,
-  club_name: "Grêmio"
-},
-{
-  id: 9,
-  club_name: "Internacional"
-},
-{
-  id: 10,
-  club_name: "Minas Brasília"
-}]
-
-const clubsExpectedResponse = [{
   id: 1,
   clubName: "Avaí/Kindermann"
 },
@@ -102,12 +60,21 @@ describe('Testa a rota /clubs', () => {
   let chaiHttpResponse: Response;
 
   before(() => {
-    sinon.stub(Clubs, 'findAll').resolves(clubsMock as IClubModel[]);
+    sinon.stub(Clubs, 'findAll')
+      .resolves(clubsMock as IClubModel[]);
+
+    sinon.stub(Clubs, 'findOne')
+      .withArgs({ where: { id: 2 } })
+      .resolves(clubsMock[1] as IClubModel)
+      .withArgs({ where: { id: 5 } })
+      .resolves(clubsMock[4] as IClubModel);
   });
 
   after(() => {
-    const clubsStub = Clubs.findAll as unknown as Sinon.SinonMock;
-    clubsStub.restore();
+    const clubsFindAllStub = Clubs.findAll as unknown as Sinon.SinonMock;
+    clubsFindAllStub.restore();
+    const clubsFindOneStub = Clubs.findOne as unknown as Sinon.SinonMock;
+    clubsFindOneStub.restore();
   });
 
   it('Deve retornar uma lista de times', async () => {
@@ -116,7 +83,7 @@ describe('Testa a rota /clubs', () => {
       .get('/clubs')
 
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.deep.equal(clubsExpectedResponse);
+    expect(chaiHttpResponse.body).to.deep.equal(clubsMock);
   });
 
   it('ao passar um id como parametro deve retornar apenas um time com aquele id', async () => {
@@ -125,13 +92,13 @@ describe('Testa a rota /clubs', () => {
       .get('/clubs/2')
 
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.deep.equal(clubsExpectedResponse[1]);
+    expect(chaiHttpResponse.body).to.deep.equal(clubsMock[1]);
 
     chaiHttpResponse = await chai
       .request(app)
       .get('/clubs/5')
 
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.deep.equal(clubsExpectedResponse[4]);
+    expect(chaiHttpResponse.body).to.deep.equal(clubsMock[4]);
   });
 })
