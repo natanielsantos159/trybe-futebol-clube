98f0511dcc32c568f10c76c342720507e2701cdd
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-26 13:41:54 -0300

    Requisito 29 - Cria interface IClubSummation

--- app/backend/src/interfaces/IClub.ts
+++ app/backend/src/interfaces/IClub.ts
@@ -9,3 +9,17 @@ export interface IClub {
   id: number;
   clubName: string;
 }
+
+export interface IClubSummation {
+  id?: number;
+  name: string;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  totalDraws: number;
+  totalVictories: number;
+  totalLosses: number;
+  totalPoints: number;
+  totalGames: number;
+  efficiency?: number;
+}
