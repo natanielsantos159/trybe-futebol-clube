6acfd4b31cc3dadabd1edd46c7f2d27023cf1b63
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-25 21:07:40 -0300

    Requisito 21 - corrige filtro por inProgress

--- app/backend/src/controllers/matchsController.ts
+++ app/backend/src/controllers/matchsController.ts
@@ -7,7 +7,7 @@ const getAll = async (req: Request, res: Response) => {
   let allMatchs = await matchsService.getAll();
 
   if ('inProgress' in req.query) {
-    const inProgress = Boolean(req.query.inProgress);
+    const inProgress = req.query.inProgress === 'true';
     allMatchs = allMatchs.filter(match => match.inProgress === inProgress);
   }
 
