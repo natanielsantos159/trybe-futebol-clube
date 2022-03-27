65760fa62806150d7921b2ba96978e275a91ef54
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-27 00:18:19 -0300

    Requisito 31 - Cria a rota leaderboard/away

--- app/backend/src/routes/leaderboardRoute.ts
+++ app/backend/src/routes/leaderboardRoute.ts
@@ -5,4 +5,6 @@ const leaderboard = Router();
 
 leaderboard.get('/home/', leaderboardController.getHomeTeamLeaderboard);
 
+leaderboard.get('/away/', leaderboardController.getAwayTeamLeaderboard);
+
 export default leaderboard
