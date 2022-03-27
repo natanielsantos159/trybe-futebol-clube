cb1c04285857af37d76534e5b27cb9c9815c82b8
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-27 00:17:59 -0300

    Requisito 31 - Implementa controller da rota leaderboard/away

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -6,6 +6,13 @@ const getHomeTeamLeaderboard = async (_req: Request, res: Response) => {
   return res.status(200).json(homeLeaderboard);
 }
 
+
+const getAwayTeamLeaderboard = async (_req: Request, res: Response) => {
+  const awayLeaderboard = await leaderboardService.getAwayTeamLeaderboard();
+  return res.status(200).json(awayLeaderboard);
+}
+
 export default {
   getHomeTeamLeaderboard,
+  getAwayTeamLeaderboard,
 }
